6cef1e3efa04fe1727413dd6438332ee9251eb6f
agmacpha <agmacpha@ualberta.ca>
2026-09-10 10:53:16 -0600

    remove test file paths

--- R/acoustic-pre-processing.R
+++ R/acoustic-pre-processing.R
@@ -969,8 +969,7 @@ wt_songscope_tags <- function (input, output = c("env","csv"), output_file=NULL,
 
 wt_guano_tags <- function(path, output = FALSE, output_file = NULL) {
 
-  wav_path <- "tests/testthat/fixtures/audio/BAT1_2426_20250724_003827_000.wav"
-  wav_path <- "/users/alexandremacphail/desktop/BCP11-BWRNNE_20240629_001828_000.wav"
+  wav_path <- path
   con <- file(wav_path, "rb")
   on.exit(close(con), add = TRUE)
 
